b1b66d68cd196ecf54792320e484b4443093f9c3
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-15 13:03:26 +0200

    0. Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,6 +7,7 @@ of indexes to return in a list for those particular pagination parameters.
 
 
 def index_range(page, page_size):
+    '''Page numbers are 1-indexed, i.e. the first page is page 1.'''
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
