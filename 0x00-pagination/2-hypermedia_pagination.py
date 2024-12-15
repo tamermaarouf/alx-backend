9673f066cfe4e1ff85a34aba4e7731ebfe264623
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-15 17:55:26 +0200

    2. Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+'''
+Implement a method named get_page that takes two integer arguments
+page with default value 1 and page_size with default value 10.
+You have to use this CSV file
+Use assert to verify that both arguments are integers greater than 0.
+Use index_range to find the correct indexes to paginate the
+dataset correctly and return the appropriate page of
+the dataset (i.e. the correct list of rows). If the input arguments are
+out of range for the dataset, an empty list should be returned.
+'''
+
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    '''Page numbers are 1-indexed, i.e. the first page is page 1.'''
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    '''Server class to paginate a database of popular baby names.'''
+    DATA_FILE = 'Popular_Baby_Names.csv'
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        '''Cached dataset'''
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''Retrieves a page of data.'''
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+        if start_index > len(data):
+            return []
+        return (data[start_index:end_index])
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = (total_items + page_size - 1) // page_size
+        details = {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages
+        }
+        return details
+        pass
