e982ab400044b3e5d7dcc2abe9a60a19aa22886c
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-30 05:37:41 +0200

    0. Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 '''
-a class BasicCache that inherits from BaseCaching and is a caching system:
-You must use self.cache_data - dictionary from the parent class BaseCaching
-This caching system doesn’t have limit
-def put(self, key, item):
-Must assign to the dictionary self.cache_data the item value for the key key.
-If key or item is None, this method should not do anything.
-def get(self, key):
-Must return the value in self.cache_data linked to key.
-If key is None or if the key doesn’t exist in self.cache_data, return None.
+a class BasicCache that inherits from BaseCaching
 '''
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    '''BasicCache that inherits from BaseCaching and is a caching system:
+    You must use self.cache_data - dictionary from the parent class BaseCaching
+    This caching system doesn’t have limit
+    '''
     def __init__(self):
+        '''Initiliaze
+        '''
         super().__init__()
 
     def put(self, key, item):
+        '''Must assign to the dictionary
+        self.cache_data the item value for the key key.
+        If key or item is None, this method should not do anything.
+        '''
         if key is not None or item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        '''Must return the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in self.cache_data,
+        '''return None.
+
         return self.cache_data.get(key) or None
