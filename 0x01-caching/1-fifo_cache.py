da41fa2c7ad645ca2d6541defb73d4cccdeffdf4
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-30 14:33:56 +0200

    1. FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -23,6 +23,15 @@ class FIFOCache(BasicCache):
         return self.cache_data.get(key, None)
 
     def put(self, key, item):
+        '''Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        If key or item is None, this method should not do anything.
+        If the number of items in self.cache_data is
+        higher that BaseCaching.MAX_ITEMS:
+        you must discard the first item put in cache (FIFO algorithm)
+        print DISCARD: with the key discarded and following by a new line
+        '''
+
         if key is not None and item is not None:
             self.cache_data[key] = item
         if len(self.cache_data) > self.MAX_ITEMS:
