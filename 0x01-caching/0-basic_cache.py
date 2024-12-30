7ea446ba5367e4cd5949178cdbd9bf1699adb3de
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-30 05:41:09 +0200

    0. Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -20,11 +20,11 @@ class BasicCache(BaseCaching):
         self.cache_data the item value for the key key.
         If key or item is None, this method should not do anything.
         '''
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         '''Must return the value in self.cache_data linked to key.
         If key is None or if the key doesn’t exist in self.cache_data,
         '''
-        return self.cache_data.get(key) or None
+        return self.cache_data.get(key, None)
