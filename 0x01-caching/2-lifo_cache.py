ca6274d3de97f4b5ab4642f195cb2d555a006532
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-31 05:42:00 +0200

    2. LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -32,9 +32,11 @@ class LIFOCache(BasicCache):
         print DISCARD: with the key discarded and following by a new line
         '''
 
-        if key is not None and item is not None:
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > self.MAX_ITEMS:
+                last_key = list(self.cache_data)[-1]
+                self.cache_data.pop(last_key)
+                print(f'DISCARD: {last_key}')
             self.cache_data[key] = item
-        if len(self.cache_data) > self.MAX_ITEMS:
-            last_key = list(self.cache_data)[-1]
-            self.cache_data.pop(last_key)
-            print(f'DISCARD: {last_key}')
